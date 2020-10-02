a7344dae99b27eebde868647fe54d2c5d63033f2
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-10-02 15:58:33 +0530

    dataURL

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Meme = (props) => {
   const [text, setText] = useState("");
   const [inarr, setInarr] = useState([1, 2]);
-
+  const [base64, setBase64] = useState();
   const addInput = () => {
     setInarr([...inarr, inarr.length + 1]);
   };
@@ -12,9 +12,31 @@ const Meme = (props) => {
     setText(e.target.value);
   };
 
+  const convertURI = (url) => {
+    const img = new Image();
+    img.setAttribute("crossOrigin", "anonymous");
+    img.onload = function () {
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+      const dataURL = canvas.toDataURL("image/png");
+      setBase64(dataURL);
+    };
+    img.src = url;
+  };
+
+  useEffect(() => {
+    convertURI(require(`../images/${props.match.params.photo}`));
+    return () => {
+      setBase64("");
+    };
+  }, []);
+
   const inputN = inarr.map((inp) => {
     return (
-      <div>
+      <div key={inp}>
         <input type="text" placeholder={`Enter text ${inp}`} />
       </div>
     );
@@ -22,7 +44,7 @@ const Meme = (props) => {
 
   return (
     <div className="meme">
-      <p>HELLO create your MEME here!!</p>
+      <p>{base64}</p>
       <div className="meme__top">
         <input
           type="text"
@@ -38,7 +60,7 @@ const Meme = (props) => {
           height="400px"
           xmlns="http://www.w3.org/2000/svg"
           xmlnsXlink="http://www.w3.org/1999/xlink"
-          style={{ backgroundColor: "pink", color: "white" }}
+          style={{ backgroundColor: "white", color: "white" }}
           className="svg"
         >
           <image
