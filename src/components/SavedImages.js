cd7c4179d0b43fa47283008918733e36c99a09f2
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-12-23 01:17:34 +0530

    shared buttons added

--- src/components/SavedImages.js
+++ src/components/SavedImages.js
@@ -1,9 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../firebase";
+import "../Style/SavedImage.css";
+
+import {
+  WhatsappShareButton,
+  WhatsappIcon,
+  FacebookIcon,
+  FacebookShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+  PinterestIcon,
+  PinterestShareButton,
+  PocketIcon,
+  PocketShareButton,
+} from "react-share";
 
 const SavedImages = (props) => {
   const [memes, setMemes] = useState();
-
+  const [url, setUrl] = useState();
   useEffect(() => {
     if (props.sign) {
       console.log(props.user.uid);
@@ -13,10 +27,60 @@ const SavedImages = (props) => {
     }
   }, []);
 
+  const share = (img) => {
+    setUrl(img);
+    document.querySelector(".share-btns").classList.add("show");
+  };
+
   return (
     <div>
       Saved Images
-      {memes ? memes.memes.map((meme) => <img src={meme} />) : null}
+      {memes
+        ? memes.memes.map((meme) => (
+            <>
+              <img src={meme} onClick={() => setUrl(meme)} />
+              <button onClick={() => share(meme)}>Share</button>
+            </>
+          ))
+        : null}
+      <div className="share-btns">
+        <WhatsappShareButton
+          url={url}
+          title={"Look at this"}
+          separator=" :: "
+          className="Demo__some-network__share-button"
+        >
+          <WhatsappIcon size={32} round />
+        </WhatsappShareButton>
+        <FacebookShareButton
+          url={url}
+          quote="Check this out : "
+          className="Demo__some-network__share-button"
+        >
+          <FacebookIcon size={32} round />
+        </FacebookShareButton>
+        <TwitterShareButton
+          url={url}
+          title="Check this out : "
+          className="Demo__some-network__share-button"
+        >
+          <TwitterIcon size={32} round />
+        </TwitterShareButton>
+        <PinterestShareButton
+          url={String(window.location)}
+          media={url}
+          className="Demo__some-network__share-button"
+        >
+          <PinterestIcon size={32} round />
+        </PinterestShareButton>
+        <PocketShareButton
+          url={url}
+          title="Check this out"
+          className="Demo__some-network__share-button"
+        >
+          <PocketIcon size={32} round />
+        </PocketShareButton>
+      </div>
     </div>
   );
 };
